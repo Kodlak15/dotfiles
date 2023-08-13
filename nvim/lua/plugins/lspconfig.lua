cfc943de741bf991b2f19e2bb06495518e0cf745
Kodlak15 <stanlcod15@gmail.com>
2023-08-13 01:25:47 -0700

    Work on audio widget

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- change a keymap
     -- keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
     -- disable a keymap
