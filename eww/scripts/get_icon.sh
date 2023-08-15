841fe5ed100e5806c22145429936ded207cd05a7
Kodlak15 <stanlcod15@gmail.com>
2023-08-15 01:34:01 -0700

    Refactor scripts

--- eww/scripts/get_icon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-OWCLI="HOME/bin/openweathercli"
-
-icon="$($OWCLI --print icon)"
-echo "$icon"
--- eww/scripts/is_laptop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ ! -z $(ls /sys/class/power_supply/) ]]; then
-    echo true
-else
-    echo false
-fi
