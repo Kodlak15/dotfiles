655d6a1224ca96c36c27305686071e59a487f20d
Kodlak15 <stanlcod15@gmail.com>
2023-10-06 02:28:30 -0700

    Improve eww bar

--- eww/scripts/open_links.sh
+++ eww/scripts/open_links.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-cmd="/usr/bin/librewolf --new-window"
+cmd="/usr/bin/librewolf --new-tab"
 
 close_dashboard() {
 	"$HOME/dotfiles/eww/scripts/dashboard.sh" -t "$HOME/dotfiles/eww/bars/default"
 }
 
 case $1 in
---gh)
-	$cmd "https://github.com"
-	close_dashboard
-	;;
---rd)
-	$cmd "https://reddit.com"
-	close_dashboard
-	;;
---gt)
-	$cmd "https://gentoo.org"
-	close_dashboard
-	;;
-*)
-	$cmd
-	close_dashboard
-	;;
+--gh) $cmd "https://github.com" && close_dashboard ;;
+--rd) $cmd "https://reddit.com" && close_dashboard ;;
+--gt) $cmd "https://gentoo.org" && close_dashboard ;;
+*) $cmd && close_dashboard ;;
 esac
